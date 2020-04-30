3742f281fa6e678d00ce8bf446943cd4c09b4bde
Aisha Keita <aishakeita6@gmail.com>
2020-04-30 16:30:53 +0200

    jQuery draft

---
+++ jquery.js
@@ -0,0 +1,55 @@
+
+// You should create a page with the accordion using jQuery. You should use CSS to style all the things and using jQuery write logic that will open/hide content below each section (Location, Music, Notes, Books, Tendances)  when clicking on the section title
+
+$('.location_container').on('click', () =>{
+    // $('.location-text').slideToggle();
+    $('.content_text').slideToggle();
+});
+
+$('#content_container').on('click', () => {
+   
+});
+
+$('.music_container').on('click', () => {
+    // $('.music-text').slideToggle();
+    $('.content_text').slideToggle();
+});
+
+$('.note_container').on('click', () => {
+    $('.content_text').slideToggle();
+    // $('.note-text').slideToggle();
+});
+
+$('.content_container').on('click', () => {
+    // $('.content-text').slideToggle();
+});
+
+$('.book_container').on('click', () => {
+    // $('.book-text').slideToggle();
+    $('.content_text').slideToggle();
+});
+
+$('.tendances_container').on('click', ()=>{
+    // $('.tendances-text').slideToggle();
+    $('.content_text').slideToggle();
+});
+
+// let listItem = document.getElementByTagname ('li');
+
+//dollar sign and (name of the tag you are  calling) then.on ('click', () => consolole.log(true);
+// for(listItem of listItem){
+    //listItem.addEventlistner('click', fuunction())
+//}
+
+//to get a button with the id my button
+//you could do
+// let currentElement;
+// $(`#my-buttin`).on('click', () => {
+// if (!currentElement){
+//     //store the first child
+//     currentElement = $ (`my-list li:first-child`)
+// }else
+// {
+//     //get the sibling of the current element
+// }
+// });
